5a38f9a0080876bd057bbc30cb9904a3f43b248b
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-04-23 00:32:36 +0800

    Let the user input response if he/she is adding another line

--- multiple_line_text.py
+++ multiple_line_text.py
@@ -11,15 +11,17 @@ while response_loop_ctrl == 0:
     response_loop_ctrl += 1
 
 # Ask the user for input
-    user_input = str (input ("Enter a line of text: "))
+    user_input = str(input("Enter a line of text: "))
 
 # Create and open a file named mylife.txt
     with open ("mylife.txt", "a") as multiple_text:
+        
+        # Append the user's input to mylife.txt and separate it by new line
+        multiple_text.write (str(user_input) + "\n")
 
-        # Append the user's input to mylife.txt and separate it to new line
-        multiple_text.write (str (user_input) + "\n")
-        
-        
-# Ask the user if he/she wants add another line
+        # Ask the user if he/she wants add another line
+        user_response = str(input("Do you want to enter another line y/n? "))
+        response_loop_ctrl = 0
+
 # Display error message if the input is more than 1 character
 # Design the output using tkinter
