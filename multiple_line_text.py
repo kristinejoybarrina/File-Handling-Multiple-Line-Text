84a440d131636cc472f3ac3363e46b23e3b6866a
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-04-23 00:14:46 +0800

    Initialize a loop control then create a while loop

--- multiple_line_text.py
+++ multiple_line_text.py
@@ -2,7 +2,15 @@
 # Creating a python program that writes multiple line of text contents to another text file.
 
 # Pseudocode
+
+# Initialize loop control
+response_loop_ctrl = 0
+
 # Use while loop
+while response_loop_ctrl == 0:
+    print ("its working")
+    response_loop_ctrl += 1
+
 # Ask the user for input
 # Create and open a file named mylife.txt
 # Ask the user if he/she wants add another line
