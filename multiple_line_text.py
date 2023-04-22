961a0cd15b14a4bed2fcc576b68a937c6fd3a964
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-04-23 00:10:21 +0800

    Pseudocode

---
+++ multiple_line_text.py
@@ -0,0 +1,10 @@
+# Kristine Joy Barrina #BSCPE 1-5 # April 23, 2023
+# Creating a python program that writes multiple line of text contents to another text file.
+
+# Pseudocode
+# Use while loop
+# Ask the user for input
+# Create and open a file named mylife.txt
+# Ask the user if he/she wants add another line
+# Display error message if the input is more than 1 character
+# Design the output using tkinter
