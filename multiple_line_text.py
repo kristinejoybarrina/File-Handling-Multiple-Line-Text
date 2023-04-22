a70159ffe667d5fa3fed09e3443035457c2857b3
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-04-23 00:22:53 +0800

    Append the user's input to mylife.txt

--- multiple_line_text.py
+++ multiple_line_text.py
@@ -15,9 +15,11 @@ while response_loop_ctrl == 0:
 
 # Create and open a file named mylife.txt
     with open ("mylife.txt", "a") as multiple_text:
-        print ("its working")
-        
 
+        # Append the user's input to mylife.txt and separate it to new line
+        multiple_text.write (str (user_input) + "\n")
+        
+        
 # Ask the user if he/she wants add another line
 # Display error message if the input is more than 1 character
 # Design the output using tkinter
